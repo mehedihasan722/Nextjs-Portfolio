c562461c3b54018d8ad42f087aacc7c254c90596
Mehedi <mjimehedi99@gmail.com>
2024-11-01 16:12:04 +0600

    🚨 Home Page Photo added
    Nothing

---
+++ src/components/Socials.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  {
+    icon: <FaGithub />,
+    path: "",
+  },
+  {
+    icon: <FaLinkedin />,
+    path: "",
+  },
+  {
+    icon: <FaYoutube />,
+    path: "",
+  },
+  {
+    icon: <FaTwitter />,
+    path: "",
+  },
+];
+const Socials = ({ containerStyles, iconStyles }: any) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link href={item.path} key={index} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Socials;
