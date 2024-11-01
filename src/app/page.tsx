1d95b2d0da592064c191e12764fc19b88d96147e
Mehedi <mjimehedi99@gmail.com>
2024-11-01 09:55:22 +0600

    🔖 Home Page start
    fuck

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <main>Home Page</main>;
-}
+import React from "react";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span>Software Developer</span>
+            <h1 className="h1">
+              Hello I'm
+              <br /> <span>Mehedi Hasan</span>
+            </h1>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -42,7 +42,7 @@ const MobileNav = () => {
       <SheetTrigger className="flex justify-center items-center">
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
-      <SheetContent>
+      <SheetContent className="flex flex-col">
         {/* logo */}
         <div className="mt-32 mb-40 text-center text-2xl">
           <Link href="/">
@@ -51,6 +51,23 @@ const MobileNav = () => {
             </h1>
           </Link>
         </div>
+        {/* Navbar */}
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  "text-accent border-b-2 border-accent "
+                } text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            );
+          })}
+        </nav>
       </SheetContent>
     </Sheet>
   );
